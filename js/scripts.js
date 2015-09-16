e8777019fe7809dbae48ffeefe5f022a7ea2c5f0
Leah Klamkin & Josue Valverde <student@epicodus.com>
2015-09-16 09:46:11 -0700

    add and pass spec that assigns a player to a space on the tic-tac-toe board.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 function Player(mark) {
   this.mark = mark;
-};
+}
 
 function Space(x, y) {
   this.xCoordinate = x;
   this.yCoordinate = y;
-};
+  this.markedBy = null;
+}
+
+Space.prototype.markBy = function(player) {
+  this.markedBy = player;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -13,4 +13,15 @@ describe('Space', function() {
     expect(testSpace.xCoordinate).to.equal(2);
     expect(testSpace.yCoordinate).to.equal(3);
   });
+
+  it("lets a player mark their space on the board", function() {
+    var testPlayer1 = new Player("X");
+    var testSpace1 = new Space(2,3);
+    var testPlayer2 = new Player("Y");
+    var testSpace2 = new Space(1,3);
+    testSpace1.markBy(testPlayer1);
+    testSpace2.markBy(testPlayer2);
+    expect(testSpace1.markedBy).to.equal(testPlayer1);
+    expect(testSpace2.markedBy).to.equal(testPlayer2);
+  });
 });
