676d4ffffa168307c35a9562f2f6fa934877c2a4
abdul rahimi & tony navarro <student@epicodus.com>
2015-09-17 12:35:01 -0700

    reorder board array, start working on win conditions

---
+++ forknotes.md
@@ -0,0 +1,3 @@
+bugs found:
+
+LN 37 verticalOcount set to a non-defined verticalYcount.  Thinking in math???
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+var x = "X";
+var y = "Y";
+
 function Player(mark) {
   this.mark = mark;
 }
@@ -13,50 +16,65 @@ Space.prototype.markBy = function(player) {
 }
 
 function Board() {
-  this.spaces = [[new Space(1,1), new Space(2,1), new Space(3,1)],
-                  [new Space(1,2), new Space(2,2), new Space(3,2)],
-                  [new Space(1,3), new Space(2,3), new Space(3,3)]];
+  this.spaces = [ [new Space(0,0), new Space(0,1), new Space(0,2)],
+                  [new Space(1,0), new Space(1,1), new Space(1,2)],
+                  [new Space(2,0), new Space(2,1), new Space(2,2)] ];
 }
 
 Board.prototype.spaceAt = function(x, y) {
-  return this.spaces[y-1][x-1];
+  return this.spaces[x][y];
 }
 
 //it's not done yet
 Board.prototype.win = function() {
   var verticalXCount = 0;
   var verticalOCount = 0;
+  var xMoves = [];
+  var oMoves = [];
   var XWin = false;
   var OWin = false;
+
   this.spaces.forEach(function(space) {
-    for(var i = 0; i < space.length; ++i) {
+    for (var i = 0; i < space.length; i++) {
       if(space[i].markedBy) {
-        if(space[i].markedBy.mark === "X") {
-          verticalXCount += 1;
+        if(space[i].markedBy.mark === "X" ) {
+          xMoves.push([space[i]]);
         } else {
-          verticalYCount += 1;
+          oMoves.push(space[i]);
         }
       }
-
-      if(verticalXCount === 3) {
-        XWin = true;
-      } else if (verticalOCount === 3) {
-        OWin = true;
-      }
     }
+    debugger;
 
-    verticalXCount = 0;
-    verticalOCount = 0;
+    // for(var i = 0; i < space.length; ++i) { //OLD CODE
+    //   if(space[i].markedBy) {
+    //     if(space[i].markedBy.mark === "X") {
+    //       verticalXCount += 1;
+    //     } else {
+    //       verticalOCount += 1;
+    //     }
+    //   }
+    //
+    //   if(verticalXCount === 3) {
+    //     XWin = true;
+    //   } else if (verticalOCount === 3) {
+    //     OWin = true;
+    //   }
+    // }
+    //
+    // verticalXCount = 0;
+    // verticalOCount = 0;
   });
 
   if(XWin === true) {
-    return "PLAYER X WINS, SUCKA";
+    alert("PLAYER X WINS, SUCKA");
   } else if (OWin === true) {
-    return "player O wins, yahoo";
+    alert("player O wins, yahoo");
   }
 
 
-  //more code for horizontal wins and diagonal wins if conditions above are not met and returned
+
+  // more code for horizontal wins and diagonal wins if conditions above are not met and returned
 }
 
 $(document).ready(function() {
@@ -67,104 +85,123 @@ $(document).ready(function() {
 
   // BOX AT 1,1 //
   $("span#playerXButton-1-1").click(function() {
-    gameBoard.spaceAt(1,1).markBy(playerX);
+    gameBoard.spaceAt(0,0).markBy(playerX);
     $("#playerOButton-1-1").hide();
-    $(this).removeClass()
-    $(this).addClass('buttonClicked');
+    $('#box-1-1').hide();
+    gameBoard.win(playerX)
+    // $(this).removeClass()
+    // $(this).addClass('buttonClicked');
   });
 
   $("span#playerOButton-1-1").click(function() {
-    gameBoard.spaceAt(1,1).markBy(playerO);
+    gameBoard.spaceAt(0,0).markBy(playerO);
     $('#box-1-1').hide();
     $('#playerXButton-1-1').hide();
+    gameBoard.win(playerO)
   });
 
   // BOX AT 1,2 //
   $("span#playerXButton-1-2").click(function() {
-    gameBoard.spaceAt(1,2).markBy(playerX);
+    gameBoard.spaceAt(0,1).markBy(playerX);
     $("#playerOButton-1-2").hide();
+    gameBoard.win(playerX)
   });
 
   $("span#playerOButton-1-2").click(function() {
-    gameBoard.spaceAt(1,2).markBy(playerO);
+    gameBoard.spaceAt(0,1).markBy(playerO);
     $('#playerXButton-1-2').hide();
+    gameBoard.win(playerO)
   });
 
   // BOX AT 1,3 //
   $("span#playerXButton-1-3").click(function() {
-    gameBoard.spaceAt(1,3).markBy(playerX);
+    gameBoard.spaceAt(0,2).markBy(playerX);
     $("#playerOButton-1-3").hide();
+    gameBoard.win(playerX)
   });
 
   $("span#playerOButton-1-3").click(function() {
-    gameBoard.spaceAt(1,3).markBy(playerO);
+    gameBoard.spaceAt(0,2).markBy(playerO);
     $('#playerXButton-1-3').hide();
+    gameBoard.win(playerO)
   });
 
   // BOX AT 2,1 //
   $("span#playerXButton-2-1").click(function() {
-    gameBoard.spaceAt(2,1).markBy(playerX);
+    gameBoard.spaceAt(1,0).markBy(playerX);
     $("#playerOButton-2-1").hide();
+    gameBoard.win(playerX)
   });
 
   $("span#playerOButton-2-1").click(function() {
-    gameBoard.spaceAt(2,1).markBy(playerO);
+    gameBoard.spaceAt(1,0).markBy(playerO);
     $('#playerXButton-2-1').hide();
+    gameBoard.win(playerO)
   });
 
   // BOX AT 2,2 //
   $("span#playerXButton-2-2").click(function() {
-    gameBoard.spaceAt(2,2).markBy(playerX);
+    gameBoard.spaceAt(1,1).markBy(playerX);
     $("#playerOButton-2-2").hide();
+    gameBoard.win(playerX)
   });
 
   $("span#playerOButton-2-2").click(function() {
-    gameBoard.spaceAt(2,2).markBy(playerO);
+    gameBoard.spaceAt(1,1).markBy(playerO);
     $('#playerXButton-2-2').hide();
+    gameBoard.win(playerO)
   });
 
   // BOX AT 2,3 //
   $("span#playerXButton-2-3").click(function() {
-    gameBoard.spaceAt(2,3).markBy(playerX);
+    gameBoard.spaceAt(1,2).markBy(playerX);
     $("#playerOButton-2-3").hide();
+    gameBoard.win(playerX)
   });
 
   $("span#playerOButton-2-3").click(function() {
-    gameBoard.spaceAt(2,3).markBy(playerO);
+    gameBoard.spaceAt(1,2).markBy(playerO);
     $('#playerXButton-2-3').hide();
+    gameBoard.win(playerO)
   });
 
   // BOX AT 3,1 //
   $("span#playerXButton-3-1").click(function() {
-    gameBoard.spaceAt(3,1).markBy(playerX);
+    gameBoard.spaceAt(2,0).markBy(playerX);
     $("#playerOButton-3-1").hide();
+    gameBoard.win(playerX)
   });
 
   $("span#playerOButton-3-1").click(function() {
-    gameBoard.spaceAt(3,1).markBy(playerO);
+    gameBoard.spaceAt(2,0).markBy(playerO);
     $('#playerXButton-3-1').hide();
+    gameBoard.win(playerO)
   });
 
   // BOX AT 3,2 //
   $("span#playerXButton-3-2").click(function() {
-    gameBoard.spaceAt(3,2).markBy(playerX);
+    gameBoard.spaceAt(2,1).markBy(playerX);
     $("#playerOButton-3-2").hide();
+    gameBoard.win(playerX)
   });
 
   $("span#playerOButton-3-2").click(function() {
-    gameBoard.spaceAt(3,2).markBy(playerO);
+    gameBoard.spaceAt(2,1).markBy(playerO);
     $('#playerXButton-3-2').hide();
+    gameBoard.win(playerO)
   });
 
   // BOX AT 3,3 //
   $("span#playerXButton-3-3").click(function() {
-    gameBoard.spaceAt(3,3).markBy(playerX);
+    gameBoard.spaceAt(2,2).markBy(playerX);
     $("#playerOButton-3-3").hide();
+    gameBoard.win(playerX)
   });
 
   $("span#playerOButton-3-3").click(function() {
-    gameBoard.spaceAt(3,3).markBy(playerO);
+    gameBoard.spaceAt(2,2).markBy(playerO);
     $('#playerXButton-3-3').hide();
+    gameBoard.win(playerO)
   });
 
 });
