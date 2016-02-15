fa27071e5d3ec826eec167985359882ac31d9e5e
afr114 <afr114@gmail.com>
2016-02-14 22:12:28 -0800

    refactor code, update style and html

--- js/scripts.js
+++ js/scripts.js
@@ -1,207 +1,96 @@
-function Player(mark) {
-  this.mark = mark;
-}
+var board = [];
+var turn = 1;
+var round = 1;
+var player1 = new Player();
+var player2 = new Player();
+var playerTurn = player1;
 
-function Space(x, y) {
-  this.xCoordinate = x;
-  this.yCoordinate = y;
-  this.markedBy = "";
-}
 
-Space.prototype.markBy = function(player) {
-  this.markedBy = player;
-}
-
-function Board() {
-  this.spaces = [ [new Space(0,0), new Space(0,1), new Space(0,2)],
-                  [new Space(1,0), new Space(1,1), new Space(1,2)],
-                  [new Space(2,0), new Space(2,1), new Space(2,2)] ];
-}
-
-Board.prototype.spaceAt = function(x, y) {
-  return this.spaces[x][y];
-}
-
-//it's not done yet
-Board.prototype.win = function(space) {
-
-    var xWin = false
-    for(var i = 0; i < 3; i++) {
-      if ( this.spaceAt(0,0).markedBy.mark === "X" && this.spaceAt(0,1).markedBy.mark === "X" && this.spaceAt(0,2).markedBy.mark === "X" ) {
-        xWin = true;
-      } else if  ( this.spaceAt(1,0).markedBy.mark === "X" && this.spaceAt(1,1).markedBy.mark === "X" && this.spaceAt(1,2).markedBy.mark === "X" ) {
-        xWin = true;
-      } else if ( this.spaceAt(2,0).markedBy.mark === "X" && this.spaceAt(2,1).markedBy.mark === "X" && this.spaceAt(2,2).markedBy.mark === "X" ) {
-        xWin = true;
-      } else if ( this.spaceAt(0,0).markedBy.mark === "X" && this.spaceAt(1,0).markedBy.mark === "X" && this.spaceAt(2,0).markedBy.mark === "X" ) {
-        xWin = true;
-      } else if ( this.spaceAt(0,1).markedBy.mark === "X" && this.spaceAt(1,1).markedBy.mark === "X" && this.spaceAt(2,1).markedBy.mark === "X" ) {
-        xWin = true;
-      } else if ( this.spaceAt(0,2).markedBy.mark === "X" && this.spaceAt(1,2).markedBy.mark === "X" && this.spaceAt(2,2).markedBy.mark === "X" ) {
-        xWin = true;
-      } else if ( this.spaceAt(0,0).markedBy.mark === "X" && this.spaceAt(1,1).markedBy.mark === "X" && this.spaceAt(2,2).markedBy.mark === "X" ) {
-        xWin = true;
-      } else if ( this.spaceAt(0,2).markedBy.mark === "X" && this.spaceAt(1,1).markedBy.mark === "X" && this.spaceAt(2,0).markedBy.mark === "X" ) {
-        xWin = true;
-      }
+function checkRow(element1, element2, element3) {
+    if (isDefined(element1) && element1 === element2 && element2 === element3) {
+      return true;
+    } else {
+      return false;
     }
+}
 
-    if (xWin === true) {
-      alert("X Wins");
-      location.reload();
-    };
+function isDefined(element) {
+  if (element != undefined){
+    return true;
+  } else {
+    return false;
+  }
+}
 
-    var oWin = false;
-    for(var i = 0; i < 3; i++) {
-      if ( this.spaceAt(0,0).markedBy.mark === "O" && this.spaceAt(0,1).markedBy.mark === "O" && this.spaceAt(0,2).markedBy.mark === "O" ) {
-        oWin = true;
-      } else if  ( this.spaceAt(1,0).markedBy.mark === "O" && this.spaceAt(1,1).markedBy.mark === "O" && this.spaceAt(1,2).markedBy.mark === "O" ) {
-        oWin = true;
-      } else if ( this.spaceAt(2,0).markedBy.mark === "O" && this.spaceAt(2,1).markedBy.mark === "O" && this.spaceAt(2,2).markedBy.mark === "O" ) {
-        oWin = true;
-      } else if ( this.spaceAt(0,0).markedBy.mark === "O" && this.spaceAt(1,0).markedBy.mark === "O" && this.spaceAt(2,0).markedBy.mark === "O" ) {
-        oWin = true;
-      } else if ( this.spaceAt(0,1).markedBy.mark === "O" && this.spaceAt(1,1).markedBy.mark === "O" && this.spaceAt(2,1).markedBy.mark === "O" ) {
-        oWin = true;
-      } else if ( this.spaceAt(0,2).markedBy.mark === "O" && this.spaceAt(1,2).markedBy.mark === "O" && this.spaceAt(2,2).markedBy.mark === "O" ) {
-        oWin = true;
-      } else if ( this.spaceAt(0,0).markedBy.mark === "O" && this.spaceAt(1,1).markedBy.mark === "O" && this.spaceAt(2,2).markedBy.mark === "O" ) {
-        oWin = true;
-      } else if ( this.spaceAt(0,2).markedBy.mark === "O" && this.spaceAt(1,1).markedBy.mark === "O" && this.spaceAt(2,0).markedBy.mark === "O" ) {
-        oWin = true;
-      }
+function checkWin(board) {
+    if (checkRow(board[2], board[4], board[6]) || checkRow(board[0], board[4], board[8])) {
+      gameOver();
     }
+    else if (checkRow(board[0], board[1], board[2]) || checkRow(board[3], board[4], board[5]) || checkRow(board[6], board[7], board[8])  ) {
+      gameOver();
+    }
+    else if (checkRow(board[0], board[3], board[6]) || checkRow(board[1], board[4], board[7]) || checkRow(board[2], board[5], board[8])) {
+      gameOver();
+    }
+  }
 
-    if (oWin === true) {
-      alert("O Wins");
-      location.reload();
-    };
+function gameOver() {
+    player1.counter = [];
+    player2.counter = [];
+    console.log(playerTurn.playerName);
+    for (var i = 0; i < 9; i++) {
+      document.getElementById("space" + i).removeEventListener("click", listener);
+    }
+  }
 
+function checkPlayerTurn() {
+    if (turn % 2 === 0) {
+    playerTurn = player2;
+  } else {
+    playerTurn = player1;
+  }
+}
+
+function Player() {
+  this.counter = [];
+  this.record = 0;
+  this.playerName = "";
+}
+
+function assignCounters() {
+  if (round % 2 === 0) {
+    player1.counter = ["O", "O", "O", "O", "O"];
+    player1.playerName = "Player O";
+    player2.counter = ["X","X","X","X"];
+    player2.playerName = "Player X";
+  } else {
+    player1.counter = ["X", "X", "X", "X", "X"];
+    player1.playerName = "Player X";
+    player2.counter = ["O","O","O","O"];
+    player2.playerName = "Player O";
+  }
+}
+
+function gameLogic(id, elementNum){
+  if (board[elementNum] === undefined) {
+    var playerCounter = playerTurn.counter.pop();
+    $("#"+ id + " span").text(playerCounter);
+    board[elementNum] = playerCounter;
+    turn++;
+    checkWin(board);
+    checkPlayerTurn();
+  }
+}
+
+function listener() {
+    var id = $(this).attr('id');
+    var elementNum = parseInt(id.slice(-1));
+    gameLogic(id, elementNum);
 }
 
 $(document).ready(function() {
-  var gameBoard = new Board();
-  var playerX = new Player("X");
-  var playerO = new Player("O");
-  var counter = 0;
-
-  // BOX AT 1,1 //
-  $("span#playerXButton-1-1").click(function() {
-    gameBoard.spaceAt(0,0).markBy(playerX);
-    $("#playerOButton-1-1").hide();
-    $('#box-1-1').hide();
-    gameBoard.win(playerX)
-  });
-
-  $("span#playerOButton-1-1").click(function() {
-    gameBoard.spaceAt(0,0).markBy(playerO);
-    $('#box-1-1').hide();
-    $('#playerXButton-1-1').hide();
-    gameBoard.win(playerO)
-  });
-
-  // BOX AT 1,2 //
-  $("span#playerXButton-1-2").click(function() {
-    gameBoard.spaceAt(0,1).markBy(playerX);
-    $("#playerOButton-1-2").hide();
-    gameBoard.win(playerX)
-  });
-
-  $("span#playerOButton-1-2").click(function() {
-    gameBoard.spaceAt(0,1).markBy(playerO);
-    $('#playerXButton-1-2').hide();
-    gameBoard.win(playerO)
-  });
-
-  // BOX AT 1,3 //
-  $("span#playerXButton-1-3").click(function() {
-    gameBoard.spaceAt(0,2).markBy(playerX);
-    $("#playerOButton-1-3").hide();
-    gameBoard.win(playerX)
-  });
-
-  $("span#playerOButton-1-3").click(function() {
-    gameBoard.spaceAt(0,2).markBy(playerO);
-    $('#playerXButton-1-3').hide();
-    gameBoard.win(playerO)
-  });
-
-  // BOX AT 2,1 //
-  $("span#playerXButton-2-1").click(function() {
-    gameBoard.spaceAt(1,0).markBy(playerX);
-    $("#playerOButton-2-1").hide();
-    gameBoard.win(playerX)
-  });
-
-  $("span#playerOButton-2-1").click(function() {
-    gameBoard.spaceAt(1,0).markBy(playerO);
-    $('#playerXButton-2-1').hide();
-    gameBoard.win(playerO)
-  });
-
-  // BOX AT 2,2 //
-  $("span#playerXButton-2-2").click(function() {
-    gameBoard.spaceAt(1,1).markBy(playerX);
-    $("#playerOButton-2-2").hide();
-    gameBoard.win(playerX)
-  });
-
-  $("span#playerOButton-2-2").click(function() {
-    gameBoard.spaceAt(1,1).markBy(playerO);
-    $('#playerXButton-2-2').hide();
-    gameBoard.win(playerO)
-  });
-
-  // BOX AT 2,3 //
-  $("span#playerXButton-2-3").click(function() {
-    gameBoard.spaceAt(1,2).markBy(playerX);
-    $("#playerOButton-2-3").hide();
-    gameBoard.win(playerX)
-  });
-
-  $("span#playerOButton-2-3").click(function() {
-    gameBoard.spaceAt(1,2).markBy(playerO);
-    $('#playerXButton-2-3').hide();
-    gameBoard.win(playerO)
-  });
-
-  // BOX AT 3,1 //
-  $("span#playerXButton-3-1").click(function() {
-    gameBoard.spaceAt(2,0).markBy(playerX);
-    $("#playerOButton-3-1").hide();
-    gameBoard.win(playerX)
-  });
-
-  $("span#playerOButton-3-1").click(function() {
-    gameBoard.spaceAt(2,0).markBy(playerO);
-    $('#playerXButton-3-1').hide();
-    gameBoard.win(playerO)
-  });
-
-  // BOX AT 3,2 //
-  $("span#playerXButton-3-2").click(function() {
-    gameBoard.spaceAt(2,1).markBy(playerX);
-    $("#playerOButton-3-2").hide();
-    gameBoard.win(playerX)
-  });
-
-  $("span#playerOButton-3-2").click(function() {
-    gameBoard.spaceAt(2,1).markBy(playerO);
-    $('#playerXButton-3-2').hide();
-    gameBoard.win(playerO)
-  });
-
-  // BOX AT 3,3 //
-  $("span#playerXButton-3-3").click(function() {
-    gameBoard.spaceAt(2,2).markBy(playerX);
-    $("#playerOButton-3-3").hide();
-    gameBoard.win(playerX)
-  });
-
-  $("span#playerOButton-3-3").click(function() {
-    gameBoard.spaceAt(2,2).markBy(playerO);
-    $('#playerXButton-3-3').hide();
-    gameBoard.win(playerO)
-  });
-
+    assignCounters();
+    for (var i = 0; i < 9; i++) {
+      document.getElementById("space" + i).addEventListener("click", listener);
+    }
 });
