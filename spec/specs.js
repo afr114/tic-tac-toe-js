6507d18b4cfeddcee650a86f54bbbee38dc4aac5
Abdul Rahimi <afr114@gmail.com>
2016-03-31 19:36:02 -0700

    update tests

--- spec/specs.js
+++ spec/specs.js
@@ -17,11 +17,29 @@ describe('isEqual', function() {
 });
 
 describe('checkRow', function() {
-  it("checks to see if all elements are defined then calls CheckWin", function() {
+  it("checks to see that there is no empty spaces in row then calls checkWin", function() {
       var row1 = ["X", "X", undefined];
       var row2 = ["X", "X", "X"];
-      var spy = chai.spy();
+      var functionCall = 0;
+      var stub = sinon.stub(window, "checkWin", function() { functionCall ++ });
       expect(checkRow(row1)).to.equal(false);
-      expect(spy).to.be.spy;
+      checkRow(row2)
+      expect(functionCall).to.equal(1);
+      stub.restore();
+  });
+});
+
+describe('checkWin', function() {
+  it("checks to see which player has won or if the game tied", function() {
+    var row1 = ["X", "X", "X"];
+    var row2 = ["O", "X", "O"];
+    var gameOverFunctionCall = 0;
+    var checkTieFunctionCall = 0;
+    var stub1 = sinon.stub(window, "gameOver", function() { gameOverFunctionCall++ });
+    var stub2 = sinon.stub(window, "checkTie", function() { checkTieFunctionCall++ });
+    checkWin(row2)
+    expect(checkTieFunctionCall).to.equal(1);
+    checkWin(row1);
+    expect(gameOverFunctionCall).to.equal(1);
   });
 });
